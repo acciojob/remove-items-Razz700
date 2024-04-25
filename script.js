84791b4d69a4eebe73731071c667f379f5bc6675
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-25 19:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let a=document.getElementById("colorSelect");
+let btn=document.querySelector("form>input");
+let array=[];
+            for (let i = 0; i < a.length; i++) {
+                array.push(a[i].value);  
+     }
+     function render(){
+        a.innerHTML="";
+        array.forEach((item,i)=>{
+        a.innerHTML+=`<option>${item}</option>`});
+     }
+     btn.addEventListener("click",fun);
+     function fun(){
+        array=array.filter((item,i)=>item!=a.value
+        );
+        render();
+     }
